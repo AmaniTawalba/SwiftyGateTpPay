ef758e397ea002a819a18e887e9a92e2adac484e
Amani Tawalbeh <atawalbeh@blessedtreeit.com>
2022-02-13 14:49:20 +0200

    first commit

---
+++ SwiftyGateToPay/SwiftyGateToPay.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyGateToPay.h
+//  SwiftyGateToPay
+//
+//  Created by Amani Tawalbeh on 2/13/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyGateToPay.
+FOUNDATION_EXPORT double SwiftyGateToPayVersionNumber;
+
+//! Project version string for SwiftyGateToPay.
+FOUNDATION_EXPORT const unsigned char SwiftyGateToPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGateToPay/PublicHeader.h>
+
+
---
+++ SwiftyGateTpPayExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyGateTpPayExamples
+//
+//  Created by Amani Tawalbeh on 2/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
